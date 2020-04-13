745d01890a37f7230690c12b4f6fc0f3976c535c
Philippe Fouquet <ph.fouquet@gmail.com>
2020-04-13 08:10:07 +0200

    Add new device MPL3115A2

--- src/define_plugin_sets.h
+++ src/define_plugin_sets.h
@@ -723,6 +723,8 @@ To create/register a plugin, you have to :
     #define USES_P063   // TTP229_KeyPad
     #define USES_P073   // 7DG
     #define USES_P079   // Wemos Motoshield
+
+    #define USES_P162   // MPL3115A2
 #endif
 
 
